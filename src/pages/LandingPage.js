e8495cf8fbefba2c3c24b9ae3ef8925a066bb8bd
Dhanis AL Ghifari <dhanisalghifari@gmail.com>
2020-06-16 17:25:37 +0700

    add component section categories

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import Header from 'parts/Header'
 import Hero from 'parts/Hero'
 import MostPicked from 'parts/MostPicked'
 import landingPage from 'json/landingPage.json';
+import Categories from 'parts/Categories';
 
 
 export default class LandingPage extends Component {
@@ -16,6 +17,7 @@ export default class LandingPage extends Component {
             <Header {...this.props}></Header>
             <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
             <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
+            <Categories data={landingPage.categories}/>
             </>
         )
     }
