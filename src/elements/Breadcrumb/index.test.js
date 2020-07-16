8854b7b252723d1bd4a8143eaeb00f29f1edab87
Dhanis AL Ghifari <dhanisalghifari@gmail.com>
2020-07-16 17:23:58 +0700

    add components breadcrumb

---
+++ src/elements/Breadcrumb/index.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { render } from "@testing-library/react";
+import Breadcrumb from './index';
+
+const setup = () => {
+    const breadcrumbList = [
+        { pageTitle: "Home", pageHref: "" },
+        { pageTitle: "House Details", pageHref: "" }
+    ];
+
+    const { container } = render(
+        <Router>
+            <Breadcrumb data={breadcrumbList} />
+        </Router>
+    );
+
+    const breadcrumb = container.querySelector(`.breadcrumb`);
+
+    return {
+        breadcrumb
+    };
+}
+
+test('should have <ol> with classname .breadcrumb and have text Home & House Details', () => {
+    const { breadcrumb } = setup();
+
+    expect(breadcrumb).toBeInTheDocument();
+    expect(breadcrumb).toHaveTextContent("Home");
+    expect(breadcrumb).toHaveTextContent("House Details");
+});
