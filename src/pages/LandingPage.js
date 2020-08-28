aae29ea1c4aaacb9f8c976c97e8c4284519f9c8b
Dhanis AL Ghifari <dhanisalghifari@gmail.com>
2020-08-28 15:46:10 +0700

    integration landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,33 +1,54 @@
-import React, { Component } from 'react'
-import Header from 'parts/Header'
-import Hero from 'parts/Hero'
-import MostPicked from 'parts/MostPicked'
-import landingPage from 'json/landingPage.json';
-import Categories from 'parts/Categories';
-import Testimony from 'parts/Testimony';
-import Footer from 'parts/Footer';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Header from "parts/Header";
+import Hero from "parts/Hero";
+import MostPicked from "parts/MostPicked";
+import Categories from "parts/Categories";
+import Testimony from "parts/Testimony";
+import Footer from "parts/Footer";
+import { fetchPage } from "store/actions/page";
 
-export default class LandingPage extends Component {
-    constructor(props) {
-        super(props);
-        this.refMostPicked = React.createRef();
-    }
+class LandingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.refMostPicked = React.createRef();
+  }
 
-    componentDidMount(){
-        window.title = "Staycation | Home";
-        window.scrollTo(0, 0);
-    }
-    
-    render() {
-        return (
-            <>
-            <Header {...this.props}></Header>
-            <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
-            <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
-            <Categories data={landingPage.categories}/>
-            <Testimony data={landingPage.testimonial} />
-            <Footer />
-            </>
-        )
-    }
+  componentDidMount() {
+    window.title = "Staycation | Home";
+    window.scrollTo(0, 0);
+
+    if (!this.props.page.landingPage)
+      this.props.fetchPage(
+        `${process.env.REACT_APP_HOST}/api/v1/member/landing-page`,
+        "landingPage"
+      );
+  }
+
+  render() {
+    const { page } = this.props;
+    const { landingPage } = page || {};
+
+    if (!page.hasOwnProperty("landingPage")) return null;
+
+    return (
+      <>
+        <Header {...this.props}></Header>
+        <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
+        <Categories data={landingPage.category} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer />
+      </>
+    );
+  }
 }
+
+const mapStateToProps = (state) => ({
+  page: state.page,
+});
+
+export default connect(mapStateToProps, { fetchPage })(LandingPage);
--- src/parts/Categories.js
+++ src/parts/Categories.js
@@ -3,19 +3,16 @@ import Button from 'elements/Button'
 import Fade from 'react-reveal/Fade';
 
 export default function Categories({data}) {
+    
     return data.map((category, index) => {
+        if (category.itemId.length === 0) return null;
         return <section className="container" key={`category-${index}`}>
             <Fade bottom>
             <h4 className="mb-3 font-weight-medium">
                 {category.name}
             </h4>
             <div className="container-grid">
-                {
-                    category.items.length === 0 ? <div className="row">
-                        <div className="col-auto align-items-center">
-                            There is no property at this category
-                        </div>
-                    </div> : category.items.map ((item, idx) => {
+                { category.itemId.map ((item, idx) => {
                         return <div className="item column-3 row-1" key={`category-${index}-item-${idx}`}>
                                <Fade bottom delay={300 * idx}>
                                 <div className="card">
@@ -25,11 +22,13 @@ export default function Categories({data}) {
                                         </div>
                                     )}
                                     <figure className="img-wrapper" style={{height: 180}}>
-                                        <img src={item.imageUrl} alt={item.name} className="img-cover"/>
+                                        <img src={item.imageId[0] 
+                                        ? `${process.env.REACT_APP_HOST}/${item.imageId[0].imageUrl}` : ""} 
+                                        alt={item.name} className="img-cover"/>
                                     </figure>
                                     <div className="meta-wrapper">
                                         <Button type="link" href={`/properties/${item._id}`} className="stretched-link d-block text-gray-800">
-                                        <h5 className="h4">{item.name}</h5>
+                                        <h5 className="h4">{item.title}</h5>
                                         </Button>
                                         <span className="text-gray-500">
                                             {item.city}, {item.country}
